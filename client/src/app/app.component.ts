2bc6887204dc6d32cbc12ce24f52e811c0e44618
Chemichanga7 <anwork05@gmail.com>
2024-02-02 16:44:46 +0300

    довёл до рабочего состояния

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,27 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'socket-client';
   message = '';
+  messages: string[] = []
   socket;
   constructor() {
-    this.socket = io("ws://localhost:80/mqtt");
+    this.socket = io("ws://localhost:80/chat");
+    this.socket.on('message', (message) => {
+      console.log(message)
+      this.messages.push(message.data)
+    })
   }
   handleSubmitNewMessage() {
     this.socket.emit('message', { data: this.message })
--- socket-b/src/app.module.ts
+++ socket-b/src/app.module.ts
@@ -2,10 +2,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [], // Подключаемые модули (в данном случае пустой массив)
   controllers: [AppController], // Контроллеры, доступные в модуле
-  providers: [AppService], // Провайдеры (сервисы), доступные в модуле
+  providers: [AppService, ChatGateway], // Провайдеры (сервисы), доступные в модуле
 })
 export class AppModule {} // Экспортируем модуль AppModule
