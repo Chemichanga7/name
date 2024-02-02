2fe595fa6e489a395c789fad28fbb2e97aba8683
Chemichanga7 <anwork05@gmail.com>
2024-02-02 15:05:37 +0300

    commit

--- socket-c/chat-socket.js
+++ socket-c/chat-socket.js
@@ -1,22 +1,22 @@
-const socket = io("http://localhost:3000")
+const socket = io("http://localhost:3000") // Устанавливаем соединение с сервером Socket.IO по адресу "http://localhost:3000"
 
-const message = document.getElementById('message');
-const messages = document.getElementById('messages');
+const message = document.getElementById('message'); // Получаем ссылку на элемент с идентификатором 'message'
+const messages = document.getElementById('messages'); // Получаем ссылку на элемент с идентификатором 'messages'
 
-const handleSubmitNewMessage = () => {
-  socket.emit('message', { data: message.value })
+const handleSubmitNewMessage = () => { // Объявляем функцию для обработки отправки нового сообщения
+  socket.emit('message', { data: message.value }) // Отправляем сообщение на сервер Socket.IO с содержимым из поля 'message'
 }
 
-socket.on('message', ({ data }) => {
-  handleNewMessage(data);
+socket.on('message', ({ data }) => { // Устанавливаем обработчик события 'message' на сокете
+  handleNewMessage(data); // Вызываем функцию для обработки нового сообщения
 })
 
-const handleNewMessage = (message) => {
-  messages.appendChild(buildNewMessage(message));
+const handleNewMessage = (message) => { // Объявляем функцию для обработки нового сообщения
+  messages.appendChild(buildNewMessage(message)); // Добавляем новое сообщение в список сообщений
 }
 
-const buildNewMessage = (message) => {
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(message))
-  return li;
+const buildNewMessage = (message) => { // Объявляем функцию для создания нового сообщения
+  const li = document.createElement("li"); // Создаем новый элемент списка
+  li.appendChild(document.createTextNode(message)) // Добавляем текст сообщения в элемент списка
+  return li; // Возвращаем созданный элемент списка
 }
