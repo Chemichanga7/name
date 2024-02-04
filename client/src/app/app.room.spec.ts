76131a7d2152c5cd1b0f132f19717d0bc5f3c3c7
dev-LostDir <dev.rodina.mark@gmail.com>
2024-02-04 22:40:29 +0200

    Добавлена маршрутизация(пока нерабочая)

--- client/src/app/app.room.spec.ts
+++ client/src/app/app.room.spec.ts
@@ -1,27 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppRoom } from './app.room';
+import {HelloWindow} from "./app.hellowindow";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [HelloWindow],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloWindow);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'client' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloWindow);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('hello-window');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloWindow);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {HelloWindow} from "./app/app.hellowindow";
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HelloWindow, appConfig)
   .catch((err) => console.error(err));
